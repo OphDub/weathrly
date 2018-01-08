9e0489761bcf9887f8ab4e339354aec160a2dcdb
Michelle Hoffman <hoffman.michelle.e@gmail.com>
2018-01-08 12:02:58 -0700

    Basic tests for Welcome component

---
+++ test/welcome.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import Welcome from '../lib/Welcome.js';
+import { data } from '../lib/data.js';
+
+describe('TenDayForecast', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<Welcome />)
+  })
+
+  it('should exist', () => {
+    expect(wrapper).toBeDefined();
+  });
+
+  it('should render div, h1, form, and Search components', () => {
+    expect(wrapper.find('div').length).toEqual(1);
+    expect(wrapper.find('h1').length).toEqual(1);
+    expect(wrapper.find('form').length).toEqual(1);
+    expect(wrapper.find('Search').length).toEqual(1);
+  });
+
+  it('should display the text we expect', () => {
+    expect(wrapper.find('h1').text()).toEqual('Welcome to Weathrly!');
+  });
+
+})

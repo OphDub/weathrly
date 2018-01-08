c1db434f8884e56ee6b7cb3b2128b74b363d55f8
Ophus Wong <ophus.wong@gmail.com>
2018-01-08 13:56:41 -0700

    Start Search tests Change 7Hour and 10Hour components to assign key of index for each Card within render

--- test/search.test.js
+++ test/search.test.js
@@ -15,4 +15,40 @@ describe('Search', () => {
     expect(wrapper).toBeDefined()
   })
 
+  it('should have an input and button', () => {
+    expect(wrapper.find('input').length).toEqual(1);
+    expect(wrapper.find('button').length).toEqual(1);
+  });
+
+  it('should start with a searchLocation as an empty string and a suggestedCities array as an empty array', () => {
+    expect(wrapper.state()).toEqual({ searchLocation: '', suggestedCities: []})
+  });
+
+  it('should start with props', () => {
+
+  });
+
+  it('should update city state on change', () => {
+
+  });
+
+  it('should update suggestedCities state with a suggestion array on change', () => {
+
+  });
+
+  it('should suggest cities after input is >= 3', () => {
+
+  });
+
+  it('should not make suggestions when input is a zip code', () => {
+
+  });
+
+  it('should run getWeatherData with the first element in the suggestCites state array when enter is pressed', () => {
+
+  })
+
+  it('should run getWeatherData with the first element in the suggestCites state array when button is clicked', () => {
+
+  })
 })

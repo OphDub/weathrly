9d192ff3a8d92cb5798e06e0180df2d63fb8dea2
Michelle Hoffman <hoffman.michelle.e@gmail.com>
2018-01-10 16:53:11 -0700

    Fix localStorage mock in app.test.js and test the text rendered in CurrentWeather.js

--- test/currentweather.test.js
+++ test/currentweather.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { mount, shallow } from 'enzyme';
 import CurrentWeather from '../lib/CurrentWeather.js';
 import { data } from '../lib/data.js';
+import App from '../lib/App.js'
 
 describe('CurrentWeather', () => {
   let wrapper;
@@ -26,5 +27,16 @@ describe('CurrentWeather', () => {
     expect(wrapper.find('p').length).toEqual(6);
     expect(wrapper.find('img').length).toEqual(1);
   });
+
+  it('should say what we expect', () => {
+    expect(wrapper.find('.location').text()).toEqual('Louisville, KY');
+    expect(wrapper.find('.date').text()).toEqual('Wed, 20 Dec 2017 11:27:36 -0500');
+    expect(wrapper.find('.temp').text()).toEqual('46 °F');
+    expect(wrapper.find('.high').text()).toEqual('High: 51 °F');
+    expect(wrapper.find('.low').text()).toEqual('Low: 32 °F');
+    expect(wrapper.find('.summary').text()).toEqual('Sun and clouds mixed. High 51F. Winds NE at 10 to 15 mph.');
+    expect(wrapper.find('.condition').text()).toEqual('Mostly Cloudy')
+
+  })
 
 })

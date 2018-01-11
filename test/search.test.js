397ec98138aaf19eb837af592372cea24f3775ce
Michelle Hoffman <hoffman.michelle.e@gmail.com>
2018-01-10 20:09:08 -0700

    Update search.test.js

--- test/search.test.js
+++ test/search.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { mount, shallow } from 'enzyme';
 import Search from '../lib/Search.js';
-import { data } from '../lib/data.js';
+import { data } from '../test/data.js';
 import { Trie } from '@ophdub/complete-me';
 import LocalStorage from '../lib/mockLocalStorage.js';
 
